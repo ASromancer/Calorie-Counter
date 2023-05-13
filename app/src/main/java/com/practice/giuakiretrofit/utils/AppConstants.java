879565f721610ddeb22628f1bb3a563678fe9fd1
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2023-05-13 12:55:19 +0700

    update

--- app/src/main/java/com/practice/giuakiretrofit/utils/AppConstants.java
+++ app/src/main/java/com/practice/giuakiretrofit/utils/AppConstants.java
@@ -1,7 +1,7 @@
 package com.practice.giuakiretrofit.utils;
 
 public class AppConstants {
-    public static final String BASE_URL ="https://caloriestrackingapprestapis-production.up.railway.app/api/v1/";
+    public static final String BASE_URL ="https://calories-tracking-app.azurewebsites.net/api/v1/";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_USER = "ROLE_USER";
 }
